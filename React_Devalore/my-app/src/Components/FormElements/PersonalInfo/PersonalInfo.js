3db36b78783061c1872398c0cf92eaf98c649f05
Nitsan <nitsan.b5@gmail.com>
2022-04-06 12:59:55 +0300

    added Dropdown and updated README file

--- React_Devalore/my-app/src/Components/FormElements/PersonalInfo/PersonalInfo.js
+++ React_Devalore/my-app/src/Components/FormElements/PersonalInfo/PersonalInfo.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import Dropdown from '../../Dropdown/Dropdown';
 import styles from './PersonalInfo.module.css';
 
 function PersonalInfo() {
   return (
-    //I partially TailwindCSS and mostly used CSS modules because I didn't have enough time to explore TailwindCSS
+    
     <div className={styles.container}>
+      
       <form>
         <div className={styles.box}>
           <label className={styles.header}>Personal Info</label>
-          <input
+          <Dropdown/>
+
+          {/* <input
             type='text'
             className='mt-1 w-full rounded-xl border-gray-300 shadow-sm focus:ring focus:ring-grey-300 focus:ring-opacity-50'
             placeholder='Name'
@@ -19,7 +23,7 @@ function PersonalInfo() {
             className='text-align:left w-full rounded-xl mt-5 border-gray-300 shadow-sm focus:ring focus:ring-grey-300 focus:ring-opacity-50'
             placeholder='Age'
             required
-          />
+          /> */}
         </div>
       </form>
     </div>
