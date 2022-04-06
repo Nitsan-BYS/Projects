6dabb8a2bf8accb207c000a5b9a754df86e192bb
Nitsan <nitsan.b5@gmail.com>
2022-04-06 12:36:15 +0300

    fixed PersonalInfo.js file

--- React_Devalore/my-app/src/Components/FormElements/PersonalInfo/PersonalInfo.js
+++ React_Devalore/my-app/src/Components/FormElements/PersonalInfo/PersonalInfo.js
@@ -10,13 +10,13 @@ function PersonalInfo() {
           <label className={styles.header}>Personal Info</label>
           <input
             type='text'
-            className='mt-1 w-full rounded-xl border-gray-300 shadow-sm focus:border-purple-500 focus:ring focus:ring-grey-300 focus:ring-opacity-50'
+            className='mt-1 w-full rounded-xl border-gray-300 shadow-sm focus:ring focus:ring-grey-300 focus:ring-opacity-50'
             placeholder='Name'
             required
           />
           <input
-            type='text'
-            className='text-align:left mt-1 w-full rounded-xl mt-5 border-gray-300 shadow-sm focus:border-purple-500 focus:ring focus:ring-grey-300 focus:ring-opacity-50'
+            type='number'
+            className='text-align:left w-full rounded-xl mt-5 border-gray-300 shadow-sm focus:ring focus:ring-grey-300 focus:ring-opacity-50'
             placeholder='Age'
             required
           />
